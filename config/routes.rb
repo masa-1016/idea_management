ba3ba39e3d2a8b07d14b7330eb42cd9f9d4d9f05
masa-1016 <77272798+masa-1016@users.noreply.github.com>
2021-06-20 14:19:22 +0900

    ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "categories#index"
+  resources :categories, only:[:new, :create]
 end

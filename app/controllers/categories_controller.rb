29259febf5591a01fd5c7900bdcbde9a9d0f0007
masa-1016 <77272798+masa-1016@users.noreply.github.com>
2021-06-20 15:53:46 +0900

    RuboCopで修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,24 @@
 class CategoriesController < ApplicationController
   def index
   end
+
+  def new
+    @category = Category.new
+    @idea = @category.ideas.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, ideas_attributes: :body)
+  end
 end
